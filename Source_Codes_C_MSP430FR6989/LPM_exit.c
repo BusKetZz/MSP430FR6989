0cfc48d4d2341898315c81954484d0ef8910ce22
Jakub Standarski <busketz2k@gmail.com>
2019-03-13 16:23:30 +0100

    Modified: LPM_exit.c

--- Source_Codes_C_MSP430FR6989/LPM_exit.c
+++ Source_Codes_C_MSP430FR6989/LPM_exit.c
@@ -1,12 +1,38 @@
-#include <msp430.h> 
+#include <msp430.h>
 
+#define STOP_WATCHDOG           0x5A80              // Stop the watchdog timer
+#define ENABLE_PINS             0xFFFE              // Required to use inputs and outputs
+#define ACLK                    0x0100              // Timer ACLK source
+#define UP                      0x0010              // Timer UP mode
+#define SLOW                    0x00C0              // Slow down ACLK by factor of 8
 
-/**
- * main.c
- */
-int main(void)
+int main()
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
-	return 0;
+    WDTCTL  =   STOP_WATCHDOG;                      // Stop the watchdog timer
+    PM5CTL0 =   ENABLE_PINS;                        // Required to use inputs and outputs
+
+    P1DIR   =   BIT0;                               // Set pin for red LED as an output
+    P1OUT   =   0x00;                               // Make sure red LED is off to start
+
+    P9DIR   =   BIT7;                               // Set pin for green LED as an output
+    P9OUT   =   0x00;                               // Make sure green LED is off to start
+
+    TA0CCR0 =   50000;                              // 50K*200us ~ 10 seconds to ISR
+    TA0CTL  =   ACLK | UP | SLOW;                   // Set ACLK, UP mode and slow down counting
+    TA0CCTL0=   CCIE;                               // Enable interrupt for Timer0
+
+    _BIS_SR(LPM0_bits | GIE);                       // Enter Low Power Mode 0 and activate interrupts
+
+    P9OUT   =   BIT7;                               // Turn on green LED after CPU comes out of Low Power Mode
+
+    while(1);
 }
+
+// *************************************************************************************************************
+// Timer0 Interrupt Service Routine
+#pragma vector = TIMER0_A0_VECTOR
+__interrupt void Timer0_ISR(void)
+{
+    _BIC_SR(LPM0_EXIT);                             // After 10 seconds, exit Low Power Mode 0
+}
+
